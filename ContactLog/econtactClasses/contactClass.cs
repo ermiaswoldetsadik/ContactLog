4f127c0f75217c1ec5a0a60f9163f606f12b3ab5
ermias <ermiaswoldetsadik@gmail.com>
2020-06-27 13:25:24 -0400

    wrote query

---
+++ ContactLog/econtactClasses/contactClass.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactLog.econtactClasses
+{
+    class contactClass
+    {
+        public int ContactID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        public DataTable Select()
+        {
+            //database connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                //sql query
+                string sql = "SELECT = FROM tbl_contact";
+                //creating cmd using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //sql adapter
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        //insert data
+        public bool Insert (contactClass c)
+        {
+            //creat default
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                //insert data
+                string sql = "INSERT INTO tbl_contact(FirstName,LastName,ContactNo,Address,Gender) VALUES(@FirstName,@LastName,@ContactNo,@Address,@Gender)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+                //open connection
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+                return isSuccess;
+        }
+    }
+
+}
